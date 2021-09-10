9fbdcc9f8b6953575c137b19696782e1ae455d72
Tripleler <seo03069@naver.com>
2021-09-11 00:29:19 +0900

    9th

---
+++ Lv1/lv1_문자열 내 마음대로 정렬하기.py
@@ -0,0 +1,6 @@
+def solution(strings, n):
+    strings.sort()
+    answer=sorted(strings, key=lambda x : x[n])
+    return answer
+print(solution(["sun", "bed", "car"],1))
+print(solution(["abce", "abcd", "cdx"],2))
