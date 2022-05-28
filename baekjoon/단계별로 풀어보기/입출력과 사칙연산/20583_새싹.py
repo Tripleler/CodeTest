3a7cc98a389abd5209e5602e7b9ec486d47f5757
Tripleler <seo03069@naver.com>
2022-05-28 14:42:50 +0900

    문자열과 입출력

---
+++ baekjoon/단계별로 풀어보기/입출력과 사칙연산/20583_새싹.py
@@ -0,0 +1,8 @@
+# https://www.acmicpc.net/problem/25083
+
+print('''         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |''')
---
+++ baekjoon/단계별로 풀어보기/입출력과 사칙연산/2588_곱셈.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/2588
+
+A = int(input())
+B = input()
+a, b, c = map(int, B)
+print(A * c)
+print(A * b)
+print(A * a)
+print(A * int(B))
